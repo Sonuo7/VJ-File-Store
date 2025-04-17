13b275b92b4186523ecd061cf92f4b8c738b00de
Sonuo7 <rockysingh12ops@gmail.com>
2025-04-17 13:38:41 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -12,10 +12,10 @@ from plugins.dbusers import db
 from pyrogram import Client, filters, enums
 from plugins.users_api import get_user, update_user_info
 from plugins.database import get_file_details
-from pyrogram.errors import ChatAdminRequired, FloodWait
-from pyrogram.types import *
+from pyrogram.errors import *
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from utils import verify_user, check_token, check_verification, get_token
-from config import *
+from config import AUTH_CHANNEL
 import re
 import json
 import base64
@@ -25,6 +25,18 @@ logger = logging.getLogger(__name__)
 
 BATCH_FILES = {}
 
+async def is_subscribed(bot, query, channel):
+    btn = []
+    for id in channel:
+        chat = await bot.get_chat(int(id))
+        try:
+            await bot.get_chat_member(id, query.from_user.id)
+        except UserNotParticipant:
+            btn.append([InlineKeyboardButton(f'Join {chat.title}', url=chat.invite_link)])
+        except Exception as e:
+            pass
+    return btn
+    
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
